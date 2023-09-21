0066048e1067e324c5536815c54b18107de45c3a
unknown <tana.palam@gmail.com>
2023-09-21 20:46:40 +0300

    add map, marker, infoWindow

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,10 @@ import { MapLoader } from 'components/MapLoader';
 import 'react-toastify/dist/ReactToastify.css';
 
 const KEY = process.env.REACT_APP_GOOGLE_MAPS_API ?? '';
+
 const libraries = ['places'];
 const warning =
-  'Google Maps API key is missing. The cart will not work correctly until you provide the key.';
+  'Google Maps API key is missing. Application will not work correctly until you provide the key.';
 
 export const App = () => {
   const { isLoaded } = useJsApiLoader({
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,3 +1,44 @@
+import { useMemo, useState } from 'react';
+import { GoogleMap, Marker, InfoWindow } from '@react-google-maps/api';
+
 export const Map = () => {
-  return <div>Map</div>;
+  const [infoWindowOpen, setInfoWindowOpen] = useState(false);
+
+  const showInfoWindow = () => {
+    setInfoWindowOpen(true);
+  };
+
+  const location = useMemo(
+    () => ({
+      lat: 46.48101658361459,
+      lng: 30.714534081895014,
+    }),
+    []
+  );
+
+  const options = useMemo(
+    () => ({
+      mapId: '5328c82c88a1caff',
+      zoom: 10,
+      center: location,
+    }),
+    []
+  );
+
+  return (
+    <GoogleMap mapContainerClassName="map-container" options={options}>
+      <Marker
+        position={location}
+        title="Hello World!"
+        icon="https://maps.google.com/mapfiles/kml/pal2/icon10.png"
+        onClick={showInfoWindow}
+      >
+        {infoWindowOpen && (
+          <InfoWindow onCloseClick={() => setInfoWindowOpen(false)}>
+            <h1>Hi I am Info Window</h1>
+          </InfoWindow>
+        )}
+      </Marker>
+    </GoogleMap>
+  );
 };
